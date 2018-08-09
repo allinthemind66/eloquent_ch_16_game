35738a8e0a5678f0a2055c2814e5e414957bdd90
allinthemind66 <r.nilon92@gmail.com>
2018-08-09 17:17:15 -0400

    create drawActors function

--- index.js
+++ index.js
@@ -162,5 +162,16 @@ function drawGrid(level){
     class: "background",
     style: `width: ${level.width * scale}px`
   }, ...level.rows.map(row => elt("tr", {style: `height: ${scale}px`}, ...row.map(type => elt("td", {class: type})))
-  ))
+));
+}
+
+function drawActors(actors){
+  return elt("div", {}, ...actors.map(actor => {
+    let rect = elt("div", {class: `actor ${actor.type}`});
+    rect.style.width = `${actor.size.x * scale}px`;
+    rect.style.height = `${actor.size.y * scale}px`;
+    rect.style.left = `${actor.position.x * scale}px`;
+    rect.style.top = `${actor.position.y * scale}px`;
+    return rect;
+  }));
 }
