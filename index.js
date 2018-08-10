c2e26856d2b5e94833e2dd15af963f411feb78c5
allinthemind66 <r.nilon92@gmail.com>
2018-08-10 18:51:50 -0400

    add run game function

--- index.js
+++ index.js
@@ -367,6 +367,14 @@ function runLevel(level, Display) {
         resolve(state.status);
         return false;
       }
-    })
-  })
+    });
+  });
+}
+
+async function runGame(plans, Display) {
+  for(let level = 0; level < plans.length;){
+    let status = await runLevel(new Level(plans[level]), Display);
+    if(status == "won") level++;
+  }
+  console.log("You've won!");
 }
