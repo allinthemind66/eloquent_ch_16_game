10e0c0f73c2293888552fdfb7808148bc8a21b5e
allinthemind66 <r.nilon92@gmail.com>
2018-08-17 17:05:30 -0400

    add drawBackground method to CanvasDisplay prototype

--- index.js
+++ index.js
@@ -580,3 +580,25 @@ CanvasDisplay.prototype.clearDisplay = function(status) {
   }
   this.cx.fillRect(0,0,this.canvas.width, this.canvas.height)
 }
+
+let otherSprites = document.createElement('img');
+otherSprites.src = "img/sprites.png";
+
+CanvasDisplay.prototype.drawBackground = function(level) {
+  let {left, top, width, height} = this.viewport;
+  let xStart = Math.floor(left);
+  let xEnd = Math.ceil(left + width);
+  let yStart = Math.floor(top);
+  let yEnd = Math.ceil(top + height);
+
+  for(let y = yStart; y < yEnd; y++){
+    for(let x = xStart; x < xEnd; x++){
+      let tile = level.rows[y][x];
+      if(tile == "empty") continue;
+      let screenX = (x - left) * scale;
+      let screenY = (y - top) * scale;
+      let tileX = tile == "lava" ? scale : 0;
+      this.cx.drawImage(otherSprites, tileX, 0, scale, scale, screenX, screenY, scale, scale)
+    }
+  }
+}
